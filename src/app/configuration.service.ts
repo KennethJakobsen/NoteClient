70253fd523ba725eecd445a91052b8360627f5bb
Kenneth Jakobsen <kenneth.jakobsen@yahoo.com>
2017-12-21 23:57:04 +0100

    Changed protocol on remote

--- src/app/configuration.service.ts
+++ src/app/configuration.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class ConfigurationService {
 
   public get BaseUrlHttp(){
-    return "http://cluster.kenned.dk";
+    return "https://cluster.kenned.dk";
   }
    public get BaseUrlSignalR(){
     return "http://cluster.kenned.dk:2345";
